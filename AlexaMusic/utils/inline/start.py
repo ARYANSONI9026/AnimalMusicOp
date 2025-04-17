d3148a05a160caf21020aabe86bea65574100f38
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-04-17 13:18:39 +0530

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -79,14 +79,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{START_IMG_URL}"),
+                InlineKeyboardButton(text=_["S_B_6"], url="https://te.legra.ph/file/ec19cf227791a167abedc.jpg"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{START_IMG_URL}"),
+                    InlineKeyboardButton(text=_["S_B_6"], url="https://te.legra.ph/file/ec19cf227791a167abedc.jpg"),
                 ]
             )
         if OWNER:
